003a36b9105fca1809e0749c160200a1f24f206f
evelin <evelinsalsabila27@gmail.com>
2026-02-27 23:09:23 +0700

    menambahkan code javascript

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// coba collab evelin
+function validasiNama() {
+    console.log("validasiNama terpanggil");
+
+    const inputPengguna = document.getElementById("id_pengguna").value.trim();
+    const pola = /^[a-zA-Z\s]+$/; // hanya huruf dan spasi
+
+    if (inputPengguna.length === 0) {
+        alert("ID Pengguna tidak boleh kosong!");
+        return false;
+    }
+
+    if (!pola.test(inputPengguna)) {
+        alert("ID Pengguna hanya boleh berisi huruf dan spasi.");
+        return false;
+    }
+
+    alert("ID Pengguna valid!");
+    return true; 
+}
